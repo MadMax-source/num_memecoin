71d0937acd00b4f92d7d2a5d64dd6ca4febda695
MadMax-source <job.m2100819@st.futminna.edu.ng>
2025-07-22 19:39:18 +0100

    modify herobg

--- src/components/background/headerBg.tsx
+++ src/components/background/headerBg.tsx
@@ -2,7 +2,7 @@ export default function WaveBackground() {
   return (
     <div className="py-10 absolute inset-0 z-0 pointer-events-none">
       <div
-        className="w-full h-full opacity-40 text-white text-4xl font-extrabold tracking-widest"
+        className="w-full h-full opacity-20 text-white text-2xl  md:text-sm font-extrabold tracking-widest"
         style={{
           backgroundImage: `
             repeating-linear-gradient(transparent 0, transparent 30px, rgba(255,255,255,0.05) 30px, rgba(255,255,255,0.05) 60px),
@@ -20,7 +20,7 @@ export default function WaveBackground() {
       >
         <div className="w-full h-full grid grid-cols-6 gap-2 text-center">
           {Array.from({ length: 50 }, (_, i) => (
-            <div key={i}>NUM</div>
+            <div key={i}>$NUM</div>
           ))}
         </div>
       </div>
